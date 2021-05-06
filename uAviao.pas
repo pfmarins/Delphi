b1129009f7a2d808cb680c4337c3e692b95bbf0b
Patrick Fernandes Marins <patrickfermarins@gmail.com>
2021-05-05 23:25:58 -0300

    #01 - Projeto ClassesObjetos

---
+++ uAviao.pas
@@ -0,0 +1,26 @@
+unit uAviao;
+
+interface
+
+uses
+ Dialogs;
+
+type
+
+ TAviao = class
+ Descricao : string;
+ Capacidade : integer;
+ HorasVoo : integer;
+ procedure Mover;
+end;
+
+implementation
+
+{ TAviao }
+
+procedure TAviao.Mover;
+begin
+ ShowMessage(Descricao+' está voando.');
+end;
+
+end.
